2437c591649a6c5639f95af77184914e4a59eea0
Danu Varshney <danuvarshney@gmail.com>
2026-06-07 15:07:44 +0530

    day8 uplodaded.

---
+++ day8/ques31.c
@@ -0,0 +1,22 @@
+// Write a program to Print character triangle.
+// A
+// AB
+// ABC
+// ABCD
+// ABCDE
+#include <stdio.h>
+int main()
+{
+    int n = 5;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf("%c", 'A' + j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
