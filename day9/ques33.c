684ab1359cc1b31fdc925fcb3f395b28405befe0
Danu Varshney <danuvarshney@gmail.com>
2026-06-09 11:22:07 +0530

    day9 uplodaded.

---
+++ day9/ques33.c
@@ -0,0 +1,25 @@
+//  Write a program to Print reverse star pattern.
+// *****
+// ****
+// ***
+// **
+// *
+ 
+#include <stdio.h>
+
+int main()
+{
+    int n = 5;
+
+    for (int i = n; i > 0; i--)
+    {
+        for (int j = i ; j > 0; j--)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
